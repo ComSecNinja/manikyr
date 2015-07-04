70d41ec4e9b7e750f39c6f2f033166ca86103424
Timo Virkkunen <timo@comsec.ninja>
2015-07-04 19:11:45 +0300

    Update utils.go
    Create a function which blocks while waiting for image to be completely created. This is not an optimal solution and issue #4 should be prioritized.

--- utils.go
+++ utils.go
@@ -1,10 +1,41 @@
 package manikyr
 
 import (
-  "io/ioutil"
-  "path"
-  "strings"
+	"image"
+	"io/ioutil"
+	"path"
+	"strings"
+	"time"
+	
+	"github.com/disintegration/imaging"
 )
+
+func openImageWhenReady(file string) (image.Image, error) {
+	// Retry opening the image until err != image.ErrFormat
+	// or next retry would take over a minute.
+	// FIXME
+	
+	var img image.Image
+	var err error
+	var retry int
+	var t time.Duration
+
+	for {
+		t = time.Duration(1000 * (retry * 2))
+		time.Sleep(time.Millisecond * t)
+
+		img, err = imaging.Open(parentFile)
+		if err == image.ErrFormat {
+			retry = retry + 1
+			if retry*2 > 60 {
+				break
+			}
+			continue
+		}
+		return img, err
+	}
+	
+}
 
 func Subdirectories(root string) ([]string, error) {
 	var dirs []string
